fd1ca54325bc04e00d2aff2c7e498830fb16aa69
username <useremail>
2025-08-10 17:56:48 -0700

    Added Data Validation While loops

--- CIS110_Pizza_Chatbot.py
+++ CIS110_Pizza_Chatbot.py
@@ -1,16 +1,35 @@
 print("Hello, my name is Alex your virtual assistant. I will help you order a pizza")
 print("I'm going to get our order started i just need to ask a few questions first. After typing a response, press enter.")
 userName = input("\nEnter your name:  ")
+
+while len(userName) ==0:
+    userName = input("Name cannot be blank! Please enter your name: ")
+
 if userName.lower() == "keyshawn reed":
     print(f"\nMy creator, {userName}. Pleasure to serve you!")
 else:
     print(f"\nHello, {userName}. Nice to meet you!")
 size = input("\nWhat size do you want? Enter small medium or large:  ")
+while size.lower() not in ["small , "medium" , "large"]:
+    size = input("Name cannot be blank! Please, enter your name: ")
+
 flavor = input("\nEnter the flavor of pizza:  ")
+while len(userName) ==0:
+    flavor = input("Name cannot be blank! Please enter your name: ")
+
 crustType = input("\What type of crust do you want  ")
+while len(userName) ==0:
+    crustType= input("Name cannot be blank! Please enter your name: ")
+
 quantity = input("\nHow many of these do you want to order? Enter a numeric value:  ")
+
+while not quantity.isdigit():
+    quantity = input("\nValue not recognized. Please enter a numeric value: ")
+
 quantity = int(quantity)
 method = input("\nIs this carry out or delivery:  ")
+while method not in ["carry out", "delivery"]:
+    method = input("Invalid value! Please enter carry out or delivery: ")
 
 if method.lower() == "delivery":
    deliveryFee = 5
@@ -25,8 +44,7 @@ elif size.lower() == "medium":
 elif size.lower() == "large":
      pizzaCost= 17.99
 
-total = (pizzaCost * quantity) * salesTax
-print("-" * 10)
+total = (pizzaCost * quantity) * salesTax + deliveryFee
 print(f"Thank you, {userName}, for your order")
 print(f"Your {quantity} {size} {flavor} pizza(s) with {crustType} crust cost ${total:,.2f}.")
 
